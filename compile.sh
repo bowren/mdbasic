bb8f6b5be1644198655b0b237c8083914acd2981
Mark Bowren <mark@bowren.com>
2022-09-20 09:20:57 -0400

    added d64 drive attach on vice start

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 tmpx -l mdbasic.lst -i mdbasic.asm -o mdbasic.prg
 if [ $? -eq 0 ]; then 
   cp mdbasic.prg ~/c64/bowren/mdbasic.prg
-  x64sc -silent -autostartwithcolon -keybuf "sys64738\n" ~/c64/bowren/mdbasic.prg &
+  x64sc -silent -8 mdbasic.d64 -autostartwithcolon -keybuf "sys64738\n" mdbasic.prg &
 fi
